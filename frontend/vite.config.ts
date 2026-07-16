fd7c41a0069de284d231200f500cd081bd80ee65
CryingDitto <101262081+CryingDitto@users.noreply.github.com>
2026-07-16 10:59:55 +0900

    chore: track package.json files for build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,28 +1,29 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      vue(),
+      // 개발(development) 환경일 때만 devTools를 활성화합니다.
+      mode === 'development' ? vueDevTools() : [],
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-  server: {
-    proxy: {
-    '/api': {
-      target: 'http://localhost:8000',
-      changeOrigin: true,
-      // rewrite: (path) => path.replace(/^\/api/, '')
-      rewrite: (path) => path
-      }
-    }
-    }
+    server: {
+      proxy: {
+        '/api': {
+          // 로컬 개발 시에는 이전과 동일하게 작동합니다.
+          target: 'http://localhost:8000',
+          changeOrigin: true,
+          rewrite: (path) => path,
+        },
+      },
+    },
+  }
 })
